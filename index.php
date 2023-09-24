fc68caa145fce1a52c3368022fd951cb9633e688
Arshavin <arsavinwallentyn@gmail.com>
2023-09-24 13:26:33 +0300

    Prepare App NOG for 2023 (#2)
    Replaced "2022" texts with current year.
    
    Gitignored `.vscode/sftp.json` file.
    
    Added an `.vscode/sftp.json.live.example` file.
    Allowed editing (on Live) only the `challenge/errors/` folder.

--- index.php
+++ index.php
@@ -9,8 +9,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
-    <title>BOS la NOG 2022</title>
-    <meta name="description" itemprop="description" content="BOS la Noaptea ONG-urilor 2022. Participă la coding-ul echipei Web." />
+    <title>BOS la NOG <?= date('Y') ?></title>
+    <meta name="description" itemprop="description" content="BOS la Noaptea ONG-urilor <?= date('Y') ?>. Participă la coding-ul echipei Web." />
     <meta name="expires" content="never">
     <meta name="revisit-after" content="1 Days">
     <meta name="google" content="notranslate" />
@@ -20,16 +20,16 @@
     <meta property="og:type" content="website" />
     <meta property="og:url" content="https://nog.bosromania.ro">
     <meta property="og:locale" content="ro_RO" />
-    <meta property="og:title" content="BOS la NOG 2022">
-    <meta property="og:description" content="BOS la Noaptea ONG-urilor 2022. Participă la coding-ul echipei Web.">
+    <meta property="og:title" content="BOS la NOG <?= date('Y') ?>">
+    <meta property="og:description" content="BOS la Noaptea ONG-urilor <?= date('Y') ?>. Participă la coding-ul echipei Web.">
     <meta property="og:image" itemprop="image" content="/assets/SEO-SM-image.jpg">
-    <meta property="og:image:alt" content="BOS la NOG 2022">
+    <meta property="og:image:alt" content="BOS la NOG <?= date('Y') ?>">
 
     <meta name="twitter:url" content="https://nog.bosromania.ro" />
-    <meta property="twitter:title" content="BOS la NOG 2022">
-    <meta property="twitter:description" content="BOS la Noaptea ONG-urilor 2022. Participă la coding-ul echipei Web.">
+    <meta property="twitter:title" content="BOS la NOG <?= date('Y') ?>">
+    <meta property="twitter:description" content="BOS la Noaptea ONG-urilor <?= date('Y') ?>. Participă la coding-ul echipei Web.">
     <meta name="twitter:image" content="/assets/SEO-SM-image.jpg" />
-    <meta property="twitter:image:alt" content="BOS la NOG 2022">
+    <meta property="twitter:image:alt" content="BOS la NOG <?= date('Y') ?>">
     <meta name="twitter:card" content="summary_large_image">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
