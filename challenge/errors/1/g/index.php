c42f75d986a065f2fd3adb47b20922dad7a8e242
Arshavin <arsavinwallentyn@gmail.com>
2023-09-25 19:53:36 +0300

    Bugfix: error "e" actually has no error (#3)
    QR replaced.
    Assets folder renamed properly.

--- challenge/errors/1/g/index.php
+++ challenge/errors/1/g/index.php
@@ -1,11 +1,13 @@
 <div style="display: inline-block; text-align: center;">
+
     <a href="https://facebook.com/bosromania" target="_blank">
-        <img src="/assets/1.7/facebook.png" width="10%" />
+        <img src="/assets/for-g/facebook.png" width="10%" />
     </a>
     <a href="https://instagraaam.com/bosromania" target="_blank">
-        <img src="/assets/1.7/instagram.png" width="10%" />
+        <img src="/assets/for-g/instagram.png" width="10%" />
     </a>
     <a href="https://tiktok.com/@bosromania" target="_blank">
-        <img src="/assets/1.7/tiktok.png" width="10%" />
+        <img src="/assets/for-g/tiktok.png" width="10%" />
     </a>
+
 </div>
